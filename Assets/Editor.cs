c1c28d81cd2988a98b635f22ec39cfada6faca9c
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-05-29 02:38:06 +0530

    The Implemetation is working correctly - buttons working correctly - other gameobjects successsfully gets destroyed when one is instantiated -working perfectly .

---
+++ Assets/Editor.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateAssetBundles : Editor
+{
+    private const string V = "C:/Users/KIIT/Desktop/AssetBundle";
+
+    [MenuItem("Assets/Create Asset Bundle")]
+    static void ExportBundle()
+    {
+        BuildPipeline.BuildAssetBundles(V, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+  
+    }
+
+}
---
+++ Assets/LoadAssetBundles.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+    AssetBundle assetBundle;
+    public string path;
+    //public string assetName;
+    IEnumerator coroutine;
+    AssetBundle bundle;
+
+  UnityEngine.Object Tree;
+  UnityEngine.Object Skeleton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DownloadAndCache());
+      
+
+    }
+
+    public void loadTree()
+    {
+        Tree = Instantiate(bundle.LoadAsset("Tree"));
+        Destroy(Skeleton);
+        
+        
+    }
+
+    public void loadSkeleton()
+    {
+        Skeleton = Instantiate(bundle.LoadAsset("Skeleton"));
+        Destroy(Tree);
+    }
+
+    IEnumerator DownloadAndCache()
+    {
+        // Wait for the Caching system to be ready
+        while (!Caching.ready)
+            yield return null;
+
+        // Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
+        using (WWW www = WWW.LoadFromCacheOrDownload(path,1))
+        {
+            yield return www;
+            if (www.error != null)
+                throw new Exception("WWW download had an error:" + www.error);
+            bundle = www.assetBundle;
+
+        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
+    }
+
+
+}
